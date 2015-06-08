64972501a8b02d18013c5002221932f0913409f4
Arjun Venkataswamy <avenkat2@gmail.com>
2015-06-08 12:29:27 -0500

    ajaxify destroy, create and edit

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -8,11 +8,27 @@ class ToDosController < ApplicationController
     respond_to do |format|
       if @to_do.save
         format.html { redirect_to root_url }
+        format.js
       else
         format.html { redirect_to root_url, notice: 'Your to-do item was invalid'}
       end
     end
 
+  end
+
+  def destroy
+    @to_do = ToDo.find(params[:id])
+    @to_do.destroy
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def edit
+    @to_do = ToDo.find(params[:id])
+    respond_to do |format|
+      format.js
+    end
   end
 
   private
